2e4dbd2e79c9f62b443643374d118127f5d2c7cf
Srishti Gupta <gsrishti747@gmail.com>
2026-02-10 13:13:26 +0530

    Minor frontend updates

--- frontend/src/landingpage/product/Hero.jsx
+++ frontend/src/landingpage/product/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return ( 
        <div className='container-fluid'>
             <div className='row text-center'>
-                    <h2 className='mt-5'>Open a Zerodha Account</h2>
+                    <h2 className='mt-5'>Open a  Account</h2>
                 <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
                 <p>Check out our investment offerings →</p>
                 </div>
